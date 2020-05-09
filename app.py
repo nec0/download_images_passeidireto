b878b7f4e6718e51d57a8183511b23b3dfc470db
neco <josehenriqueneco@gmail.com>
2020-05-09 04:17:56 -0300

    finalizado, por enquanto...

--- app.py
+++ app.py
@@ -1,104 +1,80 @@
 import time
-
-from PIL import Image
-from io import BytesIO
+import os
+import urllib.request
+import img2pdf
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.firefox.options import Options
 
 
-def get_info(url, quantidade_paginas):
-    url = url
-
+def open_url(url, nome, quant_paginas):
     option = Options()
     option.headless = True
     browser = webdriver.Chrome(chrome_options=option)
     # driver = webdriver.Firefox(options=option)
 
     browser.maximize_window()
-
-    page = url
-    info = browser.find_element_by_xpath(/html/body/div[1]/div[1]/div[2]/main/div/div/div/div/div[2]/div[1]/div/div[3]/div/div)
+    browser.get(url)
     
+    src = browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div[1]/div/div/div[1]/div/div/img").get_attribute("src")
+    info = src[41:77]
 
-    return info
+    os.mkdir(nome)
+
+    for page in range(1, quant_paginas+1):
+        url_ = f'https://files.passeidireto.com/{info}/bg{page}.png'
+        browser.get(url_)
+
+        time.sleep(3)
+        src = browser.find_element_by_xpath("/html/body/img").get_attribute("src")
+
+        time.sleep(2)
+        urllib.request.urlretrieve(src, f'./{nome}/{nome}_{page}.png') 
+        print(f'{nome}_{page}.png -> Salva')
 
 
-def save_screenshot(driver, largura, altura, file_name):
-    driver.set_window_size(largura, altura)
-    img_binary = driver.get_screenshot_as_png()
-    img = Image.open(BytesIO(img_binary))
-    img.save(file_name)
+def img_pdf(nome):
+    # A4
+    a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+    layout_fun = img2pdf.get_layout_fun(a4inpt)
 
-    print(f"{file_name} salva!")
-
-
-def open_url(email, senha, lista_urls_paginas, isbn, nome_livro, largura, altura):
-    option = Options()
-    option.headless = True
-    driver = webdriver.Chrome(chrome_options=option)
-    # driver = webdriver.Firefox(options=option)
-
-    driver.maximize_window()
-    login(driver, email, senha, isbn)
-    
-    for page in range(0, len(lista_urls_paginas)):
-        driver.get(lista_urls_paginas[page])
-        time.sleep(6)
-        save_screenshot(driver, largura, altura, f'{nome_livro}_{page}.png') 
+    dirname = str(os.path.abspath(os.path.dirname(__file__))+f"/{nome}")
+    with open(f"{nome}.pdf","wb") as f:
+        imgs = []
+        for fname in os.listdir(dirname):
+            if not fname.endswith(".png"):
+                continue
+            path = os.path.join(dirname, fname)
+            if os.path.isdir(path):
+                continue
+            imgs.append(path)
+        f.write(img2pdf.convert(imgs, layout_fun=layout_fun))
 
 
 def main():
-    #  Entradas de dados:
-    print("\nDOWNLOAD DE IMAGENS DE PÁGINAS DA BIBLIOTECA VITALSOURCE/CENGAGEBRASIL")
-    print("Antes de tudo, leia o README para não ocorrer erros!\n")
-    print("- Desenvolvido com intuito de estudo de web-scraping, recomendo que não comercialize ou matenha cópias não autorizadas de livros")
-    print("-> Foi verificado um limite de acessos, após este limite é necessário preencher um captcha(ainda não consegui quebralo(pois é do Google xD)\n")
-    
-    continuar = str(input('Continuar? (sim/não)\n')).upper()
+    print(("\nDOWNLOAD DE IMAGENS DO PASSEIDIRETO\n\n")
+            +('Passo a passo:\n')
+            +('[1] Insira a Url do material\n')
+            +('[2] Nome para identificar as imagens baixadas\n')
+            +('[3] Quantidade de páginas\n')
+            +('[4] Reuna as imagens com auxilio da ferramenta nativa do windows, imprimir -> pdf -> salvar \n')
+            +('[+] Leia o README\n'))
 
-    if continuar == "SIM":
-        print("\n-> INFORMAÇÕES <-")
-        email = str(input("Email: "))
-        senha = str(input("Senha: "))
-        isbn = str(input("Qual o ISBN do livro: "))
-        nome_livro = str(input("Identifique o livro(ex: algebra): "))
-        pagina_inicial = int(input("Qual o número da página inicial, leia o README para mais detalhes: "))
+    time.sleep(3)
+    print('-> INFORMAÇÕES <-')
+    url = str(input('URL: '))
+    nome = str(input('Nome: '))
+    quant_paginas = int(input('Quantidade de páginas: '))
 
-        print(
-            "\nRECOMENDO TESTAR AS RESOLUÇÕES ANTES DE IMPRIMIR TODAS AS PÁGINAS!\n"
-            "PORTANTO, INSIRA UMA QUANTIDADE MENOR QUE 15 PARA TESTES")
-        total_paginas = int(input('Quantidade de páginas: '))
-        pagina_final = (pagina_inicial + total_paginas)
+    open_url(url, nome, quant_paginas)
 
-        print("\nAS PÁGINAS TEM RESOLUÇÕES DIFERENTES A DEPENDER DO LIVRO, REALIZE TESTES OU VERIFIQUE PELO HTML")
-        print(
-            "-> RESOLUÇÕES <-:\n"
-            "[1] 800 x 1065\n"
-            "[2] 1600 x 2131\n"
-            "[3] 2000 x 2664\n"
-            "[4] 2000 x 2888\n")
-
-        res = int(input("Qual resolução deseja? (Recomendo a [4]) "))
-
-        largura, altura = 0, 0
-        if res == 1:
-            largura, altura = 800, 1065
-        elif res == 2:
-            largura, altura = 1600, 2130
-        elif res == 3:
-            largura, altura = 2000, 2664
-        elif res == 4:
-            largura, altura = 2000, 2888
-
-        
-        lista_urls_paginas = url_paginas(isbn, pagina_inicial, pagina_final)
-
-        open_url(email, senha, lista_urls_paginas, isbn, nome_livro, largura, altura)
-    
+    convert_pdf = str(input('\nCONVERTER EM PDF? (SIM/NÃO) ')).upper()
+    if convert_pdf == 'SIM':
+        img_pdf(nome)
+        print('Imagens e PDF salvos!')
     else:
-        print('LEIA O README!')
-    
+        print('Ok, imagens salvas!')
+
 
 main()
